e074afc88c57eeb352f194dbc6106da8aa3a06de
hazemeldoc <ahmed20191704004@cis.asu.edu.eg>
2022-09-08 08:56:19 +0200

    rust sub

---
+++ src/DNS_query.rs
@@ -0,0 +1,44 @@
+use std::str::FromStr;
+use trust_dns_client::client::{AsyncClient, ClientHandle};
+use trust_dns_client::rr::{DNSClass, Name, RecordType};
+use trust_dns_proto::op::response_code::ResponseCode;
+
+use crate::{ENT};
+use crate::race_cond;
+
+pub async fn DNS_q(mut client:AsyncClient,subdomain:String)
+{
+    // Specify the name, note the final '.' which specifies it's an FQDN
+    let name = Name::from_str(&subdomain).unwrap();
+    let query = client.query(
+        name,
+        DNSClass::IN,
+        RecordType::A,
+    );
+    let response = query.await.unwrap();
+    let answers =response.response_code();
+    let answer2=response.answers();
+    if answers!=ResponseCode::NXDomain
+    {
+        if answer2.len()==0
+        {
+            unsafe{
+                while race_cond!=1
+                {   
+                    //println!("inrace");
+                    race_cond=1;
+                    ENT.push(subdomain.clone());
+                    //println!("pushed");
+                    race_cond=0;
+                    //println!("exit race");
+                    break;
+                }
+            }
+        }
+        else
+        {
+            println!("{}",subdomain);
+        }
+    }
+    
+}
---
+++ src/file_read.rs
@@ -0,0 +1,20 @@
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+pub fn read_file_line_by_line(filepath: &str , vec:&mut Vec<String> , domain:&str) -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut file = match File::open(filepath){
+        Ok(mut f) => f,
+        Err(err)=> panic!("unable to read from file error:{}",err.to_string()),
+    };
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let mut x=line.unwrap().to_string();
+        x.push_str(".");
+        x.push_str(domain);
+        vec.push(x);
+    }
+    Ok(())
+}
